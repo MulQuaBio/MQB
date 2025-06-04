3dc25bc79e97f36e586658bfa6b74913a9a82395
Samraat Pawar <mhasoba@gmail.com>
2025-06-04 20:08:35 +0100

    minor edits to boilerplate.sh and toc file

--- content/code/boilerplate.sh
+++ content/code/boilerplate.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-# Author: Your Name your.login@imperial.ac.uk
+#!/usr/bin/env bash
+# Author: Your Name <your.login@your.email.domain>
 # Script: boilerplate.sh
-# Desc: simple boilerplate for shell scripts
-# Arguments: none
-# Date: Oct 2018
+# Desc: Simple boilerplate for shell scripts
+# Arguments: None
+# Date: Month YYYY
 
-echo -e "\nThis is a shell script! \n" #what does -e do?
+echo -e "\nThis is a shell script! \n" # what does -e do?
 
-#exit
+# The -e option enables the interpretation of backslash escapes.
+
+exit 0 # Exit the script with a status code.
+# A status code of 0 indicates success, while any other value
+# indicates an error occurred during the script execution.
